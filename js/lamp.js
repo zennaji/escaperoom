a5d5357081696b1f71cf95407471805d3d42f050
Zakaria <302586168@student.rocmondriaan.nl>
2021-11-04 20:58:53 +0100

    test

--- js/lamp.js
+++ js/lamp.js
@@ -10,7 +10,12 @@ const question3 = document.querySelector('.question3');
 const input3 = document.querySelector('.question3 input');
 const ok3 = document.querySelector('.question3 button');
 ////
+const question4 = document.querySelector('.question3');
+
+////
 const light = document.querySelectorAll('.lamp-container img');
+const key = document.querySelector('.lamp-container p');
+
 
 
 /////
@@ -30,6 +35,16 @@ ok2.addEventListener('click', function(){
         question2.classList.add("hidden");
         question3.classList.remove("hidden");
         light[1].classList.remove("hidden");
+        key.style.opacity = "20";
+    }
+})
+ok3.addEventListener('click', function(){
+    if(input3.value === "Yg3x"){
+        
+        question3.classList.add("hidden");
+        question4.classList.remove("hidden");
+
+        
     }
 })
 
